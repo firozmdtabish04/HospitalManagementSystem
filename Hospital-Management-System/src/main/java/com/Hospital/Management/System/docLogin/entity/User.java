c890e6be2657133455ced9d0d13601f6db8af975
firozmdtabish <mdtabishfiroz04@gmail.com>
2025-10-25 12:16:11 +0530

    Bug Fix SuccesFully

--- Hospital-Management-System/src/main/java/com/Hospital/Management/System/docLogin/entity/User.java
+++ Hospital-Management-System/src/main/java/com/Hospital/Management/System/docLogin/entity/User.java
@@ -1,7 +1,8 @@
-package com.application.model;
+package com.Hospital.Management.System.docLogin.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
 
 @Entity
 public class User
