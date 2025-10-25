607df3e5883f542cc22b1c54a2b1aa7458e2e6a6
firozmdtabish <mdtabishfiroz04@gmail.com>
2025-10-25 08:40:55 +0530

    UserRegistrationService Added Succesfully

--- Hospital-Management-System/src/main/java/com/Hospital/Management/System/docLogin/controller/RegistrationController.java
+++ Hospital-Management-System/src/main/java/com/Hospital/Management/System/docLogin/controller/RegistrationController.java
@@ -2,7 +2,6 @@ package com.application.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.application.model.Doctor;
 import com.application.model.Slots;
 import com.application.model.User;
