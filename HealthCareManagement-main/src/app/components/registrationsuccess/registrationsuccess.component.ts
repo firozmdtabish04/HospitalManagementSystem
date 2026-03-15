eb97c95a584f10be06ac772c526a2ec60842765f
Tabish Firoz <mdtabishfiroz04@gmail.com>
2026-03-16 01:58:06 +0530

    registration details

--- HealthCareManagement-main/src/app/components/registrationsuccess/registrationsuccess.component.ts
+++ HealthCareManagement-main/src/app/components/registrationsuccess/registrationsuccess.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,14 +6,53 @@ import { Router } from '@angular/router';
   templateUrl: './registrationsuccess.component.html',
   styleUrls: ['./registrationsuccess.component.css']
 })
-export class RegistrationsuccessComponent implements OnInit {
+export class RegistrationsuccessComponent implements OnInit, OnDestroy {
+  redirectSeconds: number = 5;
+  private countdownInterval: any;
+  private redirectTimeout: any;
 
   constructor(private router: Router) { }
 
   ngOnInit(): void {
-    setTimeout(() => {
-      this.router.navigate(['login']);
-    }, 7000);
+    this.startRedirectTimer();
   }
 
+  startRedirectTimer(): void {
+    this.countdownInterval = setInterval(() => {
+      if (this.redirectSeconds > 0)
+      {
+        this.redirectSeconds--;
+      }
+    }, 1000);
+
+    this.redirectTimeout = setTimeout(() => {
+      this.goToLogin();
+    }, 3000);
+  }
+
+  goToLogin(): void {
+    this.clearTimers();
+    this.router.navigate(['/login']);
+  }
+
+  goToHome(): void {
+    this.clearTimers();
+    this.router.navigate(['/']);
+  }
+
+  clearTimers(): void {
+    if (this.countdownInterval)
+    {
+      clearInterval(this.countdownInterval);
+    }
+
+    if (this.redirectTimeout)
+    {
+      clearTimeout(this.redirectTimeout);
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.clearTimers();
+  }
 }
