91ac2d8c8844ed7c8303c75029788372e9b147e6
Tabish Firoz <mdtabishfiroz04@gmail.com>
2026-03-16 01:34:49 +0530

    delete button added

--- HealthcareManagement-Backend/src/main/java/com/application/service/DoctorRegistrationService.java
+++ HealthcareManagement-Backend/src/main/java/com/application/service/DoctorRegistrationService.java
@@ -3,75 +3,72 @@ package com.application.service;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import com.application.model.Doctor;
 import com.application.repository.DoctorRegistrationRepository;
 
 @Service
-public class DoctorRegistrationService 
-{
+public class DoctorRegistrationService {
+
 	@Autowired
 	private DoctorRegistrationRepository doctorRegistrationRepo;
-	
-	public Doctor saveDoctor(Doctor  doctor)
-	{
+
+	public Doctor saveDoctor(Doctor doctor) {
 		return doctorRegistrationRepo.save(doctor);
 	}
-	
-	public Doctor updateDoctorProfile(Doctor doctor)
-	{
+
+	public Doctor updateDoctorProfile(Doctor doctor) {
 		return doctorRegistrationRepo.save(doctor);
 	}
-	
-	public List<Doctor> getAllDoctors()
-	{
-		return (List<Doctor>)doctorRegistrationRepo.findAll();
+
+	public List<Doctor> getAllDoctors() {
+		return (List<Doctor>) doctorRegistrationRepo.findAll();
 	}
-	
-	public void updateStatus(String email)
-	{
+
+	@Transactional
+	public void updateStatus(String email) {
 		doctorRegistrationRepo.updateStatus(email);
 	}
-	
-	public void rejectStatus(String email)
-	{
+
+	@Transactional
+	public void rejectStatus(String email) {
 		doctorRegistrationRepo.rejectStatus(email);
 		System.out.print("rejected");
 	}
-	
-	public void updatePatientStatus(String slot, String doctorname)
-	{
+
+	@Transactional
+	public void updatePatientStatus(String slot, String doctorname) {
 		doctorRegistrationRepo.updatePatientStatus(slot, doctorname);
 	}
-	
-	public void rejectPatientStatus(String slot, String doctorname)
-	{
+
+	@Transactional
+	public void rejectPatientStatus(String slot, String doctorname) {
 		doctorRegistrationRepo.rejectPatientStatus(slot, doctorname);
 		System.out.print("rejected");
 	}
-	
-	public List<Doctor> getDoctorListByEmail(String email) 
-	{
-		return (List<Doctor>)doctorRegistrationRepo.findDoctorListByEmail(email);
-	}
-	
-	public Doctor fetchDoctorByEmail(String email)
-	{
-		return doctorRegistrationRepo.findByEmail(email);
-	}
-	
-	public Doctor fetchDoctorByDoctorname(String doctorname)
-	{
-		return doctorRegistrationRepo.findByDoctorname(doctorname);
-	}
-	
-	public Doctor fetchDoctorByEmailAndPassword(String email, String password)
-	{
-		return doctorRegistrationRepo.findByEmailAndPassword(email, password);
-	}
-	
-	public List<Doctor> fetchProfileByEmail(String email)
-	{
-		return (List<Doctor>)doctorRegistrationRepo.findProfileByEmail(email);
+
+	public List<Doctor> getDoctorListByEmail(String email) {
+		return (List<Doctor>) doctorRegistrationRepo.findDoctorListByEmail(email);
 	}
 
+	public Doctor fetchDoctorByEmail(String email) {
+		return doctorRegistrationRepo.findByEmail(email);
+	}
+
+	public Doctor fetchDoctorByDoctorname(String doctorname) {
+		return doctorRegistrationRepo.findByDoctorname(doctorname);
+	}
+
+	public Doctor fetchDoctorByEmailAndPassword(String email, String password) {
+		return doctorRegistrationRepo.findByEmailAndPassword(email, password);
+	}
+
+	public List<Doctor> fetchProfileByEmail(String email) {
+		return (List<Doctor>) doctorRegistrationRepo.findProfileByEmail(email);
+	}
+
+	@Transactional
+	public void deleteDoctor(String email) {
+		doctorRegistrationRepo.deleteByEmail(email);
+	}
 }
